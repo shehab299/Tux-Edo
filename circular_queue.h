e7e9390b57651efab6d700bd8628b2d8e4db7e53
mostafarabie5 <mostafamohammed27.mm@gmail.com>
2024-04-13 22:22:27 +0200

    Solve redefinition bug

--- circular_queue.h
+++ circular_queue.h
@@ -1,10 +1,14 @@
 #include "PCB.h"
+
+#ifndef _CircularQueue
+#define _CircularQueue
 typedef struct CircularQueue
 {
   struct PCB *head;
   struct PCB *tail;
   int count;
 } CircularQueue;
+#endif
 CircularQueue createCircularQueue();
 void addProcess(CircularQueue *queue, PCB *pcb);
 void switchRunningProcess(CircularQueue *queue);
