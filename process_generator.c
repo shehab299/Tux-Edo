5e762dbe8dceff5cf5d4e58453025278e26585b8
mostafarabie5 <mostafamohammed27.mm@gmail.com>
2024-04-17 19:43:43 +0200

    Add some needed functions to process generator

--- process_generator.c
+++ process_generator.c
@@ -1,4 +1,15 @@
 #include "headers.h"
+#include "userInput.h"
+#include "inputFile.h"
+#include <unistd.h>
+#include <string.h>
+
+#ifndef _SchedulingAlgorithm
+#define _SchedulingAlgorithm
+#define HPF 1
+#define STRN 2
+#define RR 3
+#endif
 
 void clearResources(int);
 
@@ -7,8 +18,35 @@ int main(int argc, char * argv[])
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
+    inputFile();
     // 2. Ask the user for the chosen scheduling algorithm and its parameters, if there are any.
+    int selectedAlgo = userInput();
     // 3. Initiate and create the scheduler and clock processes.
+    char *absolutePath = argv[1];
+    system("gcc clk.c -o clk");
+    system("gcc scheduler.c -o scheduler");
+    int pid = fork();
+    if (pid == -1)
+    {
+        perror("Error in forking");
+        exit(-1);
+    }
+    else if (pid == 0)
+    {
+        execl(strcat(argv[1], "/clk"), "clk", NULL);
+    }
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("Error in forking");
+        exit(-1);
+    }
+    else if (pid == 0)
+    {
+        execl(strcat(argv[1], "/scheduler"), "scheduler", NULL);
+    }
+
     // 4. Use this function after creating the clock process to initialize clock
     initClk();
     // To get time use this
@@ -23,5 +61,5 @@ int main(int argc, char * argv[])
 
 void clearResources(int signum)
 {
-    //TODO Clears all resources in case of interruption
+    // TODO Clears all resources in case of interruption
 }
