ce81e0f8599849e64c7f48dc718e654ac018cae5
jpassica <jammalpassica@gmail.com>
2024-04-21 19:19:25 +0200

    Fully implement message queue communication between ProcessGenerator and [dummy] Scheduler

--- src/DataStructures/Queue.h
+++ src/DataStructures/Queue.h
@@ -1,11 +1,10 @@
-//#include "../Includes/defs.h"
-
 
 typedef struct Queue Queue;
-
+typedef struct Node Node;
 
 void dequeue(Queue* q);
 void enqueue(Queue* q, void* element);
+void* peek(Queue*);
 Queue* createQueue();
 
 
--- src/Scheduler.c
+++ src/Scheduler.c
@@ -1,3 +1,56 @@
+#include "Includes/defs.h"
+
+int rec_val, msgQueueID;
+processMsg msg;
+
+void processMessageReceiver(int signum) {
+    while((rec_val = msgrcv(msgQueueID, &msg, sizeof(msg.newProcess), getpid() % 10000, IPC_NOWAIT)) != -1 || errno != ENOMSG) 
+    {
+        if (errno == ENOMSG) {
+            printf("Scheduler: Message queue is empty\n");
+            break;
+        }
+        
+        printf("Scheduler: Message received! with process id %d\n", msg.newProcess.id);
+    }
+/*     if (rec_val == -1)
+        perror("Error in receiving message from ProcessGen!"); */
+
+    signal(SIGUSR1, processMessageReceiver);
+}
+
+void clearResources(int signum) {
+    exit (0);
+    signal(SIGINT, clearResources);
+}
+
 int main() {
+    signal(SIGUSR1, processMessageReceiver);
+    signal(SIGINT, clearResources);
+    printf("Hello from scheduler!\n");
+
+    connectToClk();
+
+    key_t key_id = ftok("../keyfile", 65);
+    msgQueueID = msgget(key_id, 0666 | IPC_CREAT);
+
+    if (msgQueueID == -1)
+    {
+        perror("Error in creating message queue!");
+        exit(-1);
+    }
+
+    printf("Scheduler: message queue created with id %d\n", msgQueueID);
+
+    msg.mtype = getpid() % 10000;
+
+    while(getTime() < 35) 
+    {
+        sleep_ms(950);
+        printf("Scheduler still running\n");
+    }
+
+    disconnectClk(true);
+
     return 0;
 }
