22ef737788ed88713edb79a44c2d0e8abcc726f5
shehab299 <shehab299@outlook.com>
2024-05-04 10:26:33 +0300

    progress

--- IPC/simulation.js
+++ IPC/simulation.js
@@ -8,9 +8,22 @@ const startSimulation = (event , seedPath, algo, timeSlice) => {
 
     const subprocess = spawn('/home/shehab/Tux-Edo/Simulation2/pg.out', [seedPath, algo, timeSlice]);
 
+    console.log(subprocess);
+
     subprocess.stdout.on('data', (data) => {
         console.log(data.toString());
     })
+
+    subprocess.stderr.on('data', (data) => {
+        console.log(data.toString());
+    })
+
+    subprocess.on('exit', (code) => {
+        console.log(`Child exited with code ${code}`);
+        event.sender.send('simulation:exit', code);
+    });
+
+
 
 };
 
