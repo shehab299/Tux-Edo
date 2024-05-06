a020e135764604c63f107cfd3f9dc85285020c38
mostafarabie5 <mostafamohammed27.mm@gmail.com>
2024-05-07 02:47:23 +0300

    Modify Implementation of priority queue

--- Phase 2/src/DataStructures/PriorityQueue.c
+++ Phase 2/src/DataStructures/PriorityQueue.c
@@ -5,145 +5,119 @@
 
 struct PriorityQueue
 {
-    void** minHeap;
+    void **array;
     int count;
     int capacity;
-    bool (*comp)(void*,void*);
+    bool (*comp)(void *, void *);
 };
 
-
-int parentIndex(int in){
-    return ((in -1 ) / 2);
+void resize(PriorityQueue **queue)
+{
+    (*queue)->capacity *= 2;
+    PriorityQueue *newQueue = pq_create((*queue)->capacity, (*queue)->comp);
+    for (int i = 0; i < (*queue)->count; i++)
+        newQueue->array[i] = (*queue)->array[i];
+    newQueue->count = (*queue)->count;
+    free(*queue);
+    *queue = newQueue;
 }
 
-int leftIndex(int in){
-    return (2 * in) + 1;
+    void *pq_create(int size, bool (*comp)(void *, void *))
+    {
+
+        PriorityQueue *q = malloc(sizeof(struct PriorityQueue));
+        q->comp = comp;
+
+        if (size <= 0)
+        {
+            perror("queue size can't be below zero");
+            exit(-1);
+        }
+
+        q->array = malloc(sizeof(void *) * size);
+        q->count = 0;
+        q->capacity = size;
+
+        return q;
 }
 
-int rightIndex(int in){
-    return (2 * in) + 2;
-}
-
-void resize(void* _queue){
-
-    PriorityQueue* queue = (PriorityQueue*) _queue;
-    queue->minHeap = realloc(queue->minHeap,2 * queue->capacity);
-    queue->capacity *= 2;
-}
-
-void minqueueify(int key, void* _queue)
+void pq_destory(void *_queue)
 {
 
-    PriorityQueue* queue = (PriorityQueue*) _queue;
+    PriorityQueue *queue = (PriorityQueue *)_queue;
 
-    if(key >= queue->count)
-        return;
-
-    int left = leftIndex(key);
-    int right = rightIndex(key);
-    int smlest = key;
-
-    int count = queue->count;
-
-
-    if(left < count && queue->comp(queue->minHeap[left],queue->minHeap[smlest]))
-        smlest = left;
-    
-    if(right < count && queue->comp(queue->minHeap[right],queue->minHeap[smlest]))
-        smlest = right;
-
-    if(smlest != key)
-    {
-        void* temp = queue->minHeap[key];
-        queue->minHeap[key] = queue->minHeap[smlest];
-        queue->minHeap[smlest] = temp;
-    }
-}
-
-
-void* pq_create(int size,bool (*comp)(void*,void*)){
-
-    PriorityQueue* q = malloc(sizeof(struct PriorityQueue));
-    q->comp = comp;
-
-    if(size <= 0)
-    {
-        perror("queue size can't be below zero");
-        exit(-1);
-    }
-
-    q->minHeap = malloc( sizeof(void*) * size );
-    q->count = 0;
-    q->capacity = size;
-
-    return q;
-}
-
-void pq_destory(void* _queue){
-
-    PriorityQueue* queue = (PriorityQueue*) _queue;
-
-    if(queue == NULL)
+    if (queue == NULL)
     {
         perror("nothing to destory");
         exit(-1);
     }
 
-    free(queue->minHeap);
+    free(queue->array);
     free(queue);
 }
 
-int pq_size(void* _queue){
-    
-    PriorityQueue* queue = (PriorityQueue*) _queue;
+int pq_size(void *_queue)
+{
+
+    PriorityQueue *queue = (PriorityQueue *)_queue;
 
     return (*queue).count;
 }
 
-bool pq_empty(void* _queue){
+bool pq_empty(void *_queue)
+{
 
-    PriorityQueue* queue = (PriorityQueue*) _queue;
+    PriorityQueue *queue = (PriorityQueue *)_queue;
 
     return (*queue).count == 0;
 }
 
-void pq_dequeue(void* _queue){
-
-    PriorityQueue* queue = (PriorityQueue*) _queue;
-
-    queue->minHeap[0] = queue->minHeap[queue->count - 1];
-    minqueueify(0,queue);
-    queue->count--;
+void pq_dequeue(void *_queue)
+{
+    pq_remove(_queue, 0);
 }
 
-void pq_enqueue(void* _queue,void* k){
-
-    PriorityQueue* queue = (PriorityQueue*) _queue;
-    
-    if(queue->count >= queue->capacity)
-        resize(queue);
-
-    int in = queue->count;
-    
-    queue->minHeap[queue->count++] = k;
-
-    while(in != 0 && queue->comp(queue->minHeap[in],queue->minHeap[parentIndex(in)]))
+void pq_remove(void *_queue, int k)
+{
+    PriorityQueue *queue = (PriorityQueue *)_queue;
+    for (int i = k + 1; i < queue->count; i++)
     {
-        void* temp = queue->minHeap[parentIndex(in)];
-        queue->minHeap[parentIndex(in)] = queue->minHeap[in];
-        queue->minHeap[in] = temp;
-        in = parentIndex(in);
+        queue->array[i - 1] = queue->array[i];
     }
+
+    queue->count--;
+    _queue = queue;
 }
 
+void pq_enqueue(void *_queue, void *k)
+{
 
+    PriorityQueue **queue = (PriorityQueue **)_queue;
 
-void* pq_top(void* _queue){
+    if ((*queue)->count >= (*queue)->capacity)
+    {
+       resize(queue);
+    }
+    int in = (*queue)->count;
 
-    PriorityQueue* queue = (PriorityQueue*) _queue;
+    (*queue)->array[(*queue)->count++] = k;
 
-    return queue->minHeap[0];
+    int prev = in - 1;
+    while (in != 0 && (*queue)->comp(k, (*queue)->array[in - 1]))
+    {
+        (*queue)->array[in] = (*queue)->array[in - 1];
+        in--;
+    }
+    (*queue)->array[in] = k;
 }
 
+void *pq_top(void *_queue)
+{
+    return pq_at(_queue, 0);
+}
 
-
+void *pq_at(void *_queue, int index)
+{
+    PriorityQueue *queue = (PriorityQueue *)_queue;
+    return queue->array[index];
+}
--- Phase 2/src/DataStructures/PriorityQueue.h
+++ Phase 2/src/DataStructures/PriorityQueue.h
@@ -4,16 +4,20 @@ struct PriorityQueue;
 
 typedef struct PriorityQueue PriorityQueue;
 
-void* pq_create(int size,bool comp(void*,void*));
+void *pq_create(int size, bool comp(void *, void *));
 
-void pq_destroy(void* heap);
+void pq_destroy(void *heap);
 
-void pq_enqueue(void* heap , void* k);
+void pq_enqueue(void *heap, void *k);
 
-void* pq_top(void* heap);
+void *pq_top(void *heap);
 
-void pq_dequeue(void* heap);
+void *pq_at(void *heap, int index);
 
-int pq_size(void* heap);
+void pq_dequeue(void *heap);
 
-bool pq_empty(void* heap);
+void pq_remove(void *heap, int k);
+
+int pq_size(void *heap);
+
+bool pq_empty(void *heap);
