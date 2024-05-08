1099cf3d602796834ffa89ffa9740472606794b8
mostafarabie5 <mostafamohammed27.mm@gmail.com>
2024-05-08 05:04:43 +0300

    Read memory size from file

--- Phase 2/src/ProcessGen.c
+++ Phase 2/src/ProcessGen.c
@@ -98,7 +98,7 @@ void initalizeQueue(char *filePath, Queue *pQueue)
 
     fptr = safe_fopen(filePath, "r");
 
-    int id, arrival, runtime, priority;
+    int id, arrival, runtime, priority,memsize;
     char ch;
 
     while ((ch = fgetc(fptr)) != EOF)
@@ -112,13 +112,14 @@ void initalizeQueue(char *filePath, Queue *pQueue)
             if ((ch = fgetc(fptr)) != EOF)
             {
                 ungetc(ch, fptr);
-                fscanf(fptr, "%d %d %d %d", &id, &arrival, &runtime, &priority);
+                fscanf(fptr, "%d %d %d %d %d", &id, &arrival, &runtime, &priority,&memsize);
 
                 newProcess = (Process *)malloc(sizeof(Process));
                 newProcess->arrivalTime = arrival;
                 newProcess->id = id;
                 newProcess->runningTime = runtime;
                 newProcess->priority = priority;
+                newProcess->memsize = memsize;
 
                 q_enqueue(pQueue, newProcess);
 
